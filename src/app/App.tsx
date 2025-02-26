0a97cdd9da8e2c7b22be57a75fc38a7b586aedbb
b.zhekeyev <baiakhmet.zhekeyev@gmail.com>
2025-02-26 15:50:14 +0500

    feat(chapter-2): lesson-3 add svg and file loader, themeSwitcher

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -7,16 +7,12 @@ import {AppRouter} from "app/provides/router";
 import {Navbar} from "widgets/Navbar";
 
 const App = () => {
-    const {theme, toggleTheme} = useTheme()
+    const {theme} = useTheme()
 
     return (
         <div className={classNames('app',{}, [theme])}>
-            {/*<button onClick={toggleTheme}>TOGGLE Theme</button>*/}
-            {/*<Link to={'/'}>Main</Link>*/}
-            {/*<Link to={'/about'}>About</Link>*/}
             <Navbar/>
             <AppRouter/>
-            <button onClick={toggleTheme}>TOGGLE Theme</button>
         </div>
     );
 };
