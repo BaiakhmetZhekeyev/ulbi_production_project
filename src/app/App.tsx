d0c2cadd471c8737499c277b820a1e8fc72842f0
b.zhekeyev <baiakhmet.zhekeyev@gmail.com>
2025-02-26 12:50:39 +0500

    feat(chapter-2): lesson-2 add Navbar and snippet

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,16 +4,19 @@ import './styles/index.scss'
 import {useTheme} from "app/provides/ThemeProviders";
 import {classNames} from "shared/lib/classNames/classNames";
 import {AppRouter} from "app/provides/router";
+import {Navbar} from "widgets/Navbar";
 
 const App = () => {
     const {theme, toggleTheme} = useTheme()
 
     return (
         <div className={classNames('app',{}, [theme])}>
-            <button onClick={toggleTheme}>TOGGLE Theme</button>
-            <Link to={'/'}>Main</Link>
-            <Link to={'/about'}>About</Link>
+            {/*<button onClick={toggleTheme}>TOGGLE Theme</button>*/}
+            {/*<Link to={'/'}>Main</Link>*/}
+            {/*<Link to={'/about'}>About</Link>*/}
+            <Navbar/>
             <AppRouter/>
+            <button onClick={toggleTheme}>TOGGLE Theme</button>
         </div>
     );
 };
